cfbfc1e08c642c3d620ab8fb77dad2f242d68b1a
marcelo.oliveira <marcelo.oliveira@gazin.com.br>
2024-12-11 13:09:41 -0300

    feat: SplashScreen and WelcomeScreen created | Welcome components created

---
+++ app/src/main/java/com/example/nearby/ui/component/welcome/WelcomeContent.kt
@@ -0,0 +1,39 @@
+package com.example.nearby.ui.component.welcome
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.nearby.R
+import com.example.nearby.ui.theme.Typography
+
+@Composable
+fun WelcomeContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        Text("Veja como funciona: ", style = Typography.bodyLarge)
+
+        WelcomeHowItWorksTip(
+            modifier = Modifier,
+            title = "Encontre estabelecimentos",
+            subtitle = "Veja locais perto de você que são parceiros Nearby",
+            iconRes = R.drawable.ic_map_pin
+        )
+        WelcomeHowItWorksTip(
+            modifier = Modifier,
+            title = "Ative o cupom com QR Code",
+            subtitle = "Escaneie o código no estabelecimento para usar o benefício",
+            iconRes = R.drawable.ic_qrcode
+        )
+        WelcomeHowItWorksTip(
+            modifier = Modifier,
+            title = "Garanta vantagens perto de você",
+            subtitle = "Ative cupons onde estiver, em diferentes tipos de estabelecimentos",
+            iconRes = R.drawable.ic_ticket
+        )
+    }
+}
